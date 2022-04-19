db1fe7dfbee234c267ad1ae9ca389057caeae20f
Oliver Reinking <oliver@codingjungle.de>
2022-04-19 18:04:52 +0200

    Korrekturen

--- resources/js/Pages/Application/Homepage/Shared/NavLink.vue
+++ resources/js/Pages/Application/Homepage/Shared/NavLink.vue
@@ -4,8 +4,8 @@
         class="block py-2 pr-4 pl-3 cursor-pointer whitespace-nowrap mt-2"
         :class="[
             route().current(routeName)
-                ? 'text-green-500 dark:text-green-500 bg-green-200 dark:bg-gray-700 rounded md:bg-transparent md:dark:bg-transparent md:p-0'
-                : 'text-gray-500 dark:text-gray-50 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700 hover:text-green-500 dark:hover:text-green-500 md:dark:hover:bg-transparent md:hover:bg-transparent md:p-0',
+                ? 'text-green-500 dark:text-green-500 bg-green-200 dark:bg-gray-700 rounded md:bg-transparent dark:md:bg-transparent md:p-0'
+                : 'text-gray-500 dark:text-gray-50 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700 hover:text-green-500 dark:hover:text-green-500 dark:md:hover:bg-transparent md:hover:bg-transparent md:p-0',
         ]"
     >
         {{ label }}</Link
